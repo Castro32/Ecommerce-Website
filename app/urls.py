a09c0c13dcc8ed7ff732b21b587ba1343ecf9020
Castro32 <okanga.fidel@gmail.com>
2024-01-05 12:48:40 +0300

    started a new ecommerce project

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from  . import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('',views.home),
+    path('about/',views.about,name="about"),
+    path('contact/',views.contact,name="contact"),
+    path('category/<slug:val>',views.CategoryView.as_view(),name="category"),
+    path('category-title/<val>',views.CategoryTitle.as_view(),name="category-title"),
+    path('product-detail/<int:pk>',views.Product_Detail.as_view(),name="product-detail"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ app/views.py
@@ -0,0 +1,39 @@
+from  django.db.models import Count
+from django.shortcuts import render
+from django.views import View
+from . models import Product
+from . forms import CustomerRegistrationForm
+
+# Create your views here.
+
+def home(request):
+    return render (request,"app/home.html ")
+
+def about(request):
+    return render (request,"app/about.html ")
+
+def contact(request):
+    return render (request,"app/contact.html ")
+
+class CategoryView(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category=val)
+        title=Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
+        return render(request, "app/category.html",locals())
+    
+class CategoryTitle(View):
+    def get(self,request,val):
+        product = Product.objects.filter(title=val)
+        title=Product.objects.filter(category=product[0].category).values('title')
+        return render(request, "app/category.html",locals())
+
+class  Product_Detail(View):
+    def get(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, "app/productdetail.html",locals())
+    
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form=CustomerRegistrationForm()
+        return render(request, "app/customerregistration.html",locals())
+        
